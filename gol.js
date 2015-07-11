f1fb52bcd4b2ba33cc030ec6a824afa35a5ea685
syzer3@gmail.com <syzer3@gmail.com>
2015-07-12 00:12:38 +0200

    done

--- gol.js
+++ gol.js
@@ -1,31 +1,11 @@
 module.exports = function gol(jsSpark, _) {
     'use strict';
 
-    var LIVE = '*';
-    var DEAD = '.';
-
     return {
-        test: test,
         getSpinner: getSpinner,
+        getPartOfWorld: getPartOfWorld,
         calc: runAll
     };
-
-    function calc(data) {
-        console.log('hey', data);
-    }
-
-    function test() {
-        console.warn(runAll(getSpinner()));
-        return jsSpark(runAll(getSpinner()))
-            .thru(calc)
-            .run()
-            .then(function (data) {
-                console.warn('all done', data);
-            })
-            .catch(function (error) {
-                console.error(error);
-            })
-    }
 
     function getSpinner() {
         return [
@@ -38,110 +18,98 @@ module.exports = function gol(jsSpark, _) {
 
     function getPartOfWorld() {
         return [
-            '.....................................................................*............................................................',
-            '.............................................................*.*...*..............................................................',
-            '..................................................................................................................................',
-            '..............................................................*.*....*............................................................',
-            '.............................................................*..*...*.............................................................',
-            '.....................................................................*............................................................',
-            '..................................................................................................................................',
-            '.................................................................*..*.............................................................',
-            '.............................................................*....*...............................................................',
-            '.................................................................**...............................................................'
+            '************************..............................................*............................................................',
+            '.................************************.....................*.*...*..............................................................',
+            '.......************................................................................................................................',
+            '................***************................................*.*....*............................................................',
+            '.....................................*........................*..*...*.............................*...............................',
+            '.......................**************.................................*............................*...............................',
+            '.....................................************...***............................................*...............................',
+            '................................*********.........................*..*.............................................................',
+            '.......................................****...................*....*...............................................................',
+            '.........................................*****....................**...............................................................'
         ].join('\n');
     }
 
-    function getNeibours(i, m, data) {
-        return [
-            1 === (i + 1) % m ? null : data[i - m - 1],
-            data[i - m],
-            0 === (i + 1) % m ? null : data[i - m + 1],
-            1 === (i + 1) % m ? null : data[i - 1],
-            0 === (i + 1) % m ? null : data[i + 1],       // on right boundary
-            1 === (i + 1) % m ? null : data[i + m - 1],  // LEFT BOUNDARY
-            data[i + m],
-            0 === (i + 1) % m ? null : data[i + m + 1]
-        ];
-    }
+    function runAll(input) {
+        var _ = this;
+        var LIVE = '*';
+        var DEAD = '.';
 
-    function countLives(acc, curr) {
-        return LIVE === curr ? acc + 1 : acc;
-    }
-
-    function countLiveNeibours(i, m, data) {
-        return getNeibours(i, m, data)
-            .reduce(countLives, 0);
-    }
-
-    function applyRules(cell, neibours) {
-        if (neibours < 2 || neibours > 3) {
-            return DEAD;
+        // return all cells
+        function convertInput(lines) {
+            var m = lines.indexOf('\n');
+            return [lines.replace(/\n/gi, ''), m];
         }
-        if (neibours > 2 && neibours < 3) {
+
+        function countLives(acc, curr) {
+            return LIVE === curr ? acc + 1 : acc;
+        }
+
+        function getNeibours(i, m, data) {
+            return [
+                1 === (i + 1) % m ? null : data[i - m - 1],
+                data[i - m],
+                0 === (i + 1) % m ? null : data[i - m + 1],
+                1 === (i + 1) % m ? null : data[i - 1],
+                0 === (i + 1) % m ? null : data[i + 1],       // on right boundary
+                1 === (i + 1) % m ? null : data[i + m - 1],  // LEFT BOUNDARY
+                data[i + m],
+                0 === (i + 1) % m ? null : data[i + m + 1]
+            ];
+        }
+
+        function countLiveNeibours(i, m, data) {
+            return getNeibours(i, m, data)
+                .reduce(countLives, 0);
+        }
+
+        function applyRules(cell, neibours) {
+            if (neibours < 2 || neibours > 3) {
+                return DEAD;
+            }
+            if (neibours > 2 && neibours < 3) {
+                return cell;
+            }
+            if (3 === neibours) {
+                return LIVE;
+            }
             return cell;
         }
-        if (3 === neibours) {
-            return LIVE;
+
+        function nextGen(boardSize) {
+            return _(boardSize[0]).map(function (el, i) {
+                return applyRules(el, countLiveNeibours(i, boardSize[1], boardSize[0]));
+            });
         }
-        return cell;
-    }
 
-    function nextGen(boardSize) {
-        return _(boardSize[0]).map(function (el, i) {
-            return applyRules(el, countLiveNeibours(i, boardSize[1], boardSize[0]));
-        });
-    }
-
-    // maybe recursive
-    function nThGeneration(boardSize, nth) {
-        nth = nth || 1;
-        var i = 0;
-        while (i < nth) {
-            boardSize[0] = nextGen(boardSize).join('');
-            i += 1;
+        // maybe recursive
+        function nThGeneration(boardSize, nth) {
+            nth = nth || 1;
+            var i = 0;
+            while (i < nth) {
+                boardSize[0] = nextGen(boardSize).join('');
+                i += 1;
+            }
+            return boardSize;
         }
-        return boardSize;
-    }
 
-    // hardcore
-    function convertBack(array) {
-        var m = array[1];
-        return _(array[0])
-            .reduce(function addEOL(acc, el, i) {
-                if (0 === (i + 1) % m && i !== array[0].length - 1) {
-                    return acc + el + '\n';
-                }
-                return acc + el;
-            })
-    }
+        // hardcore
+        function convertBack(array) {
+            var m = array[1];
+            return _(array[0])
+                .reduce(function addEOL(acc, el, i) {
+                    if (0 === (i + 1) % m && i !== array[0].length - 1) {
+                        return acc + el + '\n';
+                    }
+                    return acc + el;
+                })
+        }
 
-    // return all cells
-    function convertInput(lines) {
-        var m = lines.indexOf('\n');
-        return [lines.replace(/\n/gi, ''), m];
-    }
-
-    function runAll(input) {
         function prepare(lines) {
             return convertBack(nThGeneration(convertInput(lines)))
         }
+
         return prepare(input);
     }
-
-    function run(input) {
-        return readLines(input, prepare);
-    }
-
-    function readLines(input, lineCallback) {
-        return input
-            .split('\n')
-            .map(function (line, i) {
-                if ('' === line) {
-                    return;
-                }
-                return lineCallback(line, i);
-            })
-            .join('\n');
-    }
-
 };
--- index.js
+++ index.js
@@ -4,79 +4,21 @@ var q = jsSpark.q;
 var _ = require('lodash');
 var gol = require('./gol')(task, _);
 
-var todos = [];
+console.log('initial\n', gol.getSpinner(), '\n');
+//console.log('\nn+1\n', gol.calc(gol.getSpinner()), '\n');
 
-console.log(gol.getSpinner());
+//console.log('initial\n', gol.getPartOfWorld(), '\n');
+//console.log('\nn+1\n', gol.calc(gol.getPartOfWorld()), '\n');
 
-task([gol.getSpinner()])
-    .thru(gol.calc)
-    .run()
-    .then(function (data) {
-        console.log('done', data);
+var todos = _.range(100)
+    .map(gol.getSpinner)
+    .map(task)
+    .map(function (task) {
+        return task
+            .thru(gol.calc)
+            .run();
     });
 
-//function giveMeNiccerResponce(str) {
-//    return "THIS IS NICCER RESPONSE " + str + ' seriusly!';
-//}
-
-// example with one job
-//task([20, 30, 40, 50])
-//    // this is executed on client side
-//    .map(function addOne(num) {
-//        return num + 1;
-//    })
-//    .filter(function isBiggerThen35(el) {
-//        return 35 <= el;
-//    })
-//    .reduce(function sumUp(sum, num) {
-//        return sum + num;
-//    })
-//    .thru(giveMeNiccerResponce)
-//    .run()
-//    .then(function (data) {
-//        // this is executed on back on server
-//        console.log(data);
-//    });
-
-//q.all(texts.map(function (el) {
-//    return task(el)
-//        .thru(bigramText)
-//        .run()
-
-//function mergeBig(texts) {
-//    return q.all(texts.map(function (el) {
-//        return task(el)
-//            .thru(bigramText)
-//            .run()
-//    })).then(function reducer(data) {
-//        //return _.mergeObjectsInArr(data);     // uncomment if u want to reduce on this worker
-//        return task(data)
-//            .thru(merger)
-//            .run();
-//    }).then(function cacheInDb(data) {
-//        dataBase = data;
-//        return data;
-//    });
-//}
-//
-//function merger(arr) {
-//    var _ = this;
-//
-//    function mergeObjectsInArr(arr) {
-//        return arr.reduce(function (acc, curr) {
-//            return _.merge(acc, curr, objectMerger);
-//        });
-//    }
-//
-//    function objectMerger(a, b) {
-//        if (a && b && _.isNumber(a) && _.isNumber(b)) {
-//            return a + b;
-//        }
-//        if (_.isArray(a)) {
-//            return a.concat(b);
-//        }
-//    }
-//
-//    return mergeObjectsInArr(arr);
-//};
-//
+q.all(todos).then(function (data) {
+    console.log('all done', data[0]);
+});
